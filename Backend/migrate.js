b88f91778f487c2cbaed82c40116e1e39943dbdb
Sidharth4032 <sidharth.eage@gmail.com>
2023-12-05 23:53:40 -0500

    Update migrate.js

--- Backend/migrate.js
+++ Backend/migrate.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
 
+// Database configuration from environment variables
 const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -7,6 +8,9 @@ const dbConfig = {
     database: process.env.DB_NAME,
 };
 
+/**
+ * Main migration script to set up database tables.
+ */
 const migrationScript = async () => {
     try {
         const connection = await mysql.createConnection(dbConfig);
@@ -17,7 +21,9 @@ const migrationScript = async () => {
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                email VARCHAR(255) UNIQUE
+                email VARCHAR(255) UNIQUE,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )
         `);
 
@@ -26,10 +32,52 @@ const migrationScript = async () => {
             CREATE TABLE IF NOT EXISTS subjects (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(255) UNIQUE NOT NULL,
-                description TEXT
+                description TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
         `);
 
+        // Events Table
+        await connection.execute(`
+            CREATE TABLE IF NOT EXISTS events (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                user_id INT NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                description TEXT,
+                start_time DATETIME NOT NULL,
+                end_time DATETIME NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            )
+        `);
+
+        // Assignments Table
+        await connection.execute(`
+            CREATE TABLE IF NOT EXISTS assignments (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                subject_id INT NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                description TEXT,
+                due_date DATETIME NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (subject_id) REFERENCES subjects(id)
+            )
+        `);
+
+        // Enrollment Table
+        await connection.execute(`
+            CREATE TABLE IF NOT EXISTS enrollments (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                user_id INT NOT NULL,
+                subject_id INT NOT NULL,
+                enrollment_date DATE NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users(id),
+                FOREIGN KEY (subject_id) REFERENCES subjects(id),
+                UNIQUE KEY user_subject (user_id, subject_id)
+            )
+        `);
+
+        // Additional tables and relationships can be added here...
 
         console.log("Migration completed successfully");
         await connection.end();
